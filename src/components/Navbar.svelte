1dd305ea93cc4ff3590fe9f5cf9b25c1e242bf54
Aniket Raj <147086590+aniketr4j@users.noreply.github.com>
2025-02-24 23:43:38 +0530

    Update Navbar.svelte

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -43,8 +43,6 @@
 			data-bs-toggle="collapse"
 			data-bs-target="#navbarNav"
 			aria-controls="navbarNav"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
 		>
 			<span class="navbar-toggler-icon"></span>
 		</button>
